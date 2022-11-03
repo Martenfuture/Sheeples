9e804d6a9fc826915d3160469fb98baf75f2a8c8
Magnus Dobner <dobner.magnus@stud.hs-fresenius.de>
2022-11-03 20:47:11 +0100

    new Camera

---
+++ Assets/Scripts/ControllerSheeps/Magnus Scripts/PlayerManager.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerManager : MonoBehaviour
+{
+    private List<PlayerInput> players = new List<PlayerInput>();
+    [SerializeField]
+    private List<Transform> startingPoints;
+    [SerializeField]
+    private List<LayerMask> playerLayers;
+
+    private PlayerInputManager playerInputManager;
+
+    private void Awake()
+    {
+        playerInputManager = FindObjectOfType<PlayerInputManager>();
+    }
+
+    private void OnEnable()
+    {
+        playerInputManager.onPlayerJoined += AddPlayer;
+    }
+
+    private void OnDisable()
+    {
+        playerInputManager.onPlayerJoined -= AddPlayer;
+    }
+
+    public void AddPlayer(PlayerInput player)
+    {
+        players.Add(player);
+
+        //need to use the parent due to the structure of the prefab
+        Transform playerParent = player.transform.parent;
+        playerParent.position = startingPoints[players.Count - 1].position;
+
+        
+
+        
+
+    }
+}
