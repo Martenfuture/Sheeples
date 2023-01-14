a9a0be88b7f75c76bbc81495445b9e77db82b92e
Dominik <schilde.dominik@gmail.com>
2023-01-14 21:44:00 +0100

    Script_for_Bridge_rotation

---
+++ Assets/Scripts/MoveBridge.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBridge : MonoBehaviour
+{
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localEulerAngles = new Vector3(0, 345, 0);
+    }
+
+    
+}
