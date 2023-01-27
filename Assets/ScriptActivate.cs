f43b7076b87e839ee03cc60612ee0f516140dccf
Dominik <schilde.dominik@gmail.com>
2023-01-27 14:40:15 +0100

    testing_script

---
+++ Assets/ScriptActivate.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptActivate : MonoBehaviour
+{
+
+    public GameObject BrückeMitGelände_02(1);
+    MoveBridge script;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        script = BrückeMitGelände_02.GetComponent<MoveBridge>();
+        script.enabled = true;
+
+
+    }
+
+  
+}
